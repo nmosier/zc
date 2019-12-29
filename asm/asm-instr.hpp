0f90793a31567d7a4fbff9e4efa1ec716d6f61f2
Nicholas Mosier <nh.mosier@gmail.com>
2019-12-29 10:58:41 -0700

    done with basic asm instructions

--- asm/asm-instr.hpp
+++ asm/asm-instr.hpp
@@ -15,21 +15,26 @@ namespace zc::z80 {
    class Instruction {
    public:
 
-      virtual const char *name() const = 0;
+      /**
+       * Name of opcode
+       */
+      const char *name() const { return name_; }
 
       /**
        * Dump assembly for instruction. 
        */
-      virtual void Emit(std::ostream& os) const = 0;
+      virtual void Emit(std::ostream& os) const;
       
       /** 
        * Number of cycles required to execute the instruction.
        */
       // virtual int cycles() const = 0;
 
-   protected:
-   };
+      Instruction(const char *name): name_(name) {}
 
+   protected:
+      const char *name_;
+   };
 
    template <Size sz>
    class BinaryInstruction: public Instruction {
@@ -71,12 +76,8 @@ namespace zc::z80 {
    template <Size sz>
    class AddInstruction: public BinaryInstruction<sz> {
    public:
-      virtual const char *name() const override { return "add"; }
-      
       template <typename... Args>
-      AddInstruction(Args... args): BinaryInstruction<sz>(args...) {}
-      
-   protected:
+      AddInstruction(Args... args): BinaryInstruction<sz>(args..., "add") {}
    };
 
    /**
@@ -84,10 +85,8 @@ namespace zc::z80 {
     */
    class AndInstruction: public BinaryInstruction<Size::BYTE> {
    public:
-      virtual const char *name() const override { return "and"; }
-
       template <typename... Args>
-      AndInstruction(Args... args): BinaryInstruction(args...) {}
+      AndInstruction(Args... args): BinaryInstruction(args..., "and") {}
    };
 
    /**
@@ -95,12 +94,8 @@ namespace zc::z80 {
     */
    class CallInstruction: public UnaryInstruction<Size::LONG> {
    public:
-      virtual const char *name() const override { return "call"; }
-
       template <typename... Args>
-      CallInstruction(Args... args): UnaryInstruction<Size::LONG>(args...) {}
-      
-   protected:
+      CallInstruction(Args... args): UnaryInstruction<Size::LONG>(args..., "call") {}
    };
 
    /**
@@ -108,10 +103,8 @@ namespace zc::z80 {
     */
    class CompInstruction: public BinaryInstruction<Size::BYTE> {
    public:
-      virtual const char *name() const override { return "cp"; }
-
       template <typename... Args>
-      CompInstruction(Args... args): UnaryInstruction<Size::LONG>(args...) {}
+      CompInstruction(Args... args): UnaryInstruction<Size::LONG>(args..., "cp") {}
    };
 
    /**
@@ -120,10 +113,8 @@ namespace zc::z80 {
    template <Size sz>
    class DecInstruction: public UnaryInstruction<sz> {
    public:
-      virtual const char *name() const override { return "dec"; }
-
       template <typename... Args>
-      DecInstruction(Args... args): UnaryInstruction<sz>(args...) {}
+      DecInstruction(Args... args): UnaryInstruction<sz>(args..., "dec") {}
    };
 
    /**
@@ -131,12 +122,8 @@ namespace zc::z80 {
     */
    class DjnzInstruction: public UnaryInstruction<Size::BYTE> {
    public:
-      virtual const char *name() const override { return "djnz"; }
-
       template <typename... Args>
-      DjnzInstruction(Args... args): UnaryInstruction<Size::BYTE>(args...) {}
-      
-   protected:
+      DjnzInstruction(Args... args): UnaryInstruction<Size::BYTE>(args..., "djnz") {}
    };
 
    /**
@@ -144,12 +131,8 @@ namespace zc::z80 {
     */
    class ExInstruction: public BinaryInstruction<Size::LONG> {
    public:
-      virtual const char *name() const override { return "ex"; }
-
       template <typename... Args>
-      ExInstruction(Args... args): BinaryInstruction<Size::LONG>(args...) {}
-      
-   protected:
+      ExInstruction(Args... args): BinaryInstruction<Size::LONG>(args..., "ex") {}
    };
 
    /**
@@ -157,10 +140,8 @@ namespace zc::z80 {
     */
    class IncInstruction: public UnaryInstruction<Size::BYTE> {
    public:
-      virtual const char *name() const override { return "inc"; }
-
       template <typename... Args>
-      IncInstruction(Args... args): UnaryInstruction<Size::BYTE>(args...) {}
+      IncInstruction(Args... args): UnaryInstruction<Size::BYTE>(args..., "inc") {}
    };
 
    /**
@@ -168,10 +149,8 @@ namespace zc::z80 {
     */
    class JumpInstruction: public UnaryInstruction<Size::LONG> {
    public:
-      virtual const char *name() const override { return "jp"; }
-
       template <typename... Args>
-      JumpInstruction(Args... args): UnaryInstruction(args...) {}
+      JumpInstruction(Args... args): UnaryInstruction(args..., "jp") {}
    protected:
    };
 
@@ -180,18 +159,16 @@ namespace zc::z80 {
     */
    class JumpCondInstruction: public JumpInstruction {
    public:
-      const Flag *flag() const { return flag_; }
-      bool state() const { return state_; }
+      const FlagState *cond() const { return cond_; }
       
       virtual void Emit(std::ostream& os) const override;
 
       template <typename... Args>
-      JumpCondInstruction(const Flag *flag, bool state, Args... args):
-         JumpInstruction(args...), flag_(flag) {}
+      JumpCondInstruction(const FlagState *cond, bool state, Args... args):
+         JumpInstruction(args...), cond_(cond) {}
       
    protected:
-      const Flag *flag_;
-      bool state_;
+      const FlagState *cond_;
    };
 
    /**
@@ -200,10 +177,17 @@ namespace zc::z80 {
    template <Size sz>
    class LoadInstruction: public BinaryInstruction<sz> {
    public:
-      virtual const char *name() const override { return "ld"; }
-
       template <typename... Args>
-      LoadInstruction(Args... args): BinaryInstruction<sz>(args...) {}
+      LoadInstruction(Args... args): BinaryInstruction<sz>(args..., "ld") {}
+   };
+
+   /**
+    * "LEA" instruction class
+    */
+   class LeaInstruction: public BinaryInstruction<Size::LONG> {
+   public:
+      template <typename... Args>
+      LeaInstruction(Args... args): BinaryInstruction<Size::LONG>(args..., "lea") {}
    };
 
    /**
@@ -211,12 +195,154 @@ namespace zc::z80 {
     */
    class OrInstruction: public BinaryInstruction<Size::BYTE> {
    public:
-      virtual const char *name() const override { return "or"; }
-
       template <typename... Args>
-      OrInstruction(Args... args): BinaryInstruction<Size::BYTE>(args...) {}
+      OrInstruction(Args... args): BinaryInstruction<Size::BYTE>(args..., "or") {}
+   };
+
+   /**
+    * "PEA" instruction class
+    */
+   class PeaInstruction: public UnaryInstruction<Size::LONG> {
+   public:
+      template <typename... Args>
+      PeaInstruction(Args... args): UnaryInstruction<Size::LONG>(args..., "pea") {}
+   };
+
+   /**
+    * "POP" instruction class
+    */
+   class PopInstruction: public UnaryInstruction<Size::LONG> {
+   public:
+      template <typename... Args>
+      PopInstruction(Args... args): UnaryInstruction<Size::LONG>(args..., "pop") {}
+   };
+
+   /**
+    * "PUSH" instruction class
+    */
+   class PushInstruction: public UnaryInstruction<Size::LONG> {
+   public:
+      template <typename... Args>
+      PushInstruction(Args... args): UnaryInstruction<Size::LONG>(args..., "pop") {}
+   };
+
+   /**
+    * "RET" instruction class
+    */
+   class RetInstruction: public Instruction {
+   public:
+      template <typename... Args>
+      RetInstruction(Args... args): Instruction(args..., "ret") {}
+   };
+
+   /**
+    * "RET cc" instruction class
+    */
+   class RetCondInstruction: public RetInstruction {
+   public:
+      const FlagState *cond() const { return cond_; }
+
+      virtual void Emit(std::ostream& os) const override;
+      
+      template <typename... Args>
+      RetCondInstruction(const FlagState *cond, Args... args):
+         RetInstruction(args...), cond_(cond) {}
+      
+   protected:
+      const FlagState *cond_;
    };
    
+   /**
+    * "RL" instruction class
+    */
+   class RlInstruction: public UnaryInstruction<Size::BYTE> {
+   public:
+      template <typename... Args>
+      RlInstruction(Args... args): UnaryInstruction<Size::BYTE>(args..., "rl") {}
+   };
+
+   /**
+    * "RLC" instruction class
+    */
+   class RlcInstruction: public UnaryInstruction<Size::BYTE> {
+   public:
+      template <typename... Args>
+      RlcInstruction(Args... args): UnaryInstruction<Size::BYTE>(args..., "rlc") {}
+   };
+
+   /**
+    * "RR" instruction class
+    */
+   class RrInstruction: public UnaryInstruction<Size::BYTE> {
+   public:
+      template <typename... Args>
+      RrInstruction(Args... args): UnaryInstruction<Size::BYTE>(args..., "rr") {}
+   };
+
+   /**
+    * "RRC" instruction class
+    */
+   class RrcInstruction: public UnaryInstruction<Size::BYTE> {
+   public:
+      template <typename... Args>
+      RrcInstruction(Args... args): UnaryInstruction<Size::BYTE>(args..., "rrc") {}
+   };
+
+   /**
+    * "SBC" instruction class
+    */
+   template <Size sz>
+   class SbcInstruction: public BinaryInstruction<sz> {
+   public:
+      template <typename... Args>
+      SbcInstruction(Args... args): BinaryInstruction<sz>(args..., "sbc") {}
+   };
+
+   /**
+    * "SLA" instruction class
+    */
+   class SlaInstruction: public UnaryInstruction<Size::BYTE> {
+   public:
+      template <typename... Args>
+      SlaInstruction(Args... args): UnaryInstruction<Size::BYTE>(args..., "sla") {}
+   };
+
+   /**
+    * "SRA" instruction class
+    */
+   class SraInstruction: public UnaryInstruction<Size::BYTE> {
+   public:
+      template <typename... Args>
+      SraInstruction(Args... args): UnaryInstruction<Size::BYTE>(args..., "sra") {}
+   };
+
+   /**
+    * "SRL" instruction class
+    */
+   class SrlInstruction: public UnaryInstruction<Size::BYTE> {
+   public:
+      template <typename... Args>
+      SrlInstruction(Args... args): UnaryInstruction<Size::BYTE>(args..., "srl") {}
+   };
+   
+   /**
+    * "SUB" instruction class
+    */
+   class SubInstruction: public BinaryInstruction<Size::BYTE> {
+   public:
+      template <typename... Args>
+      SubInstruction(Args... args): BinaryInstruction<Size::BYTE>(args..., "sub") {}
+   };
+   
+   /**
+    * "XOR" instruction class
+    */
+   class XorInstruction: public UnaryInstruction<Size::BYTE> {
+   public:
+      template <typename... Args>
+      XorInstruction(Args... args): UnaryInstruction<Size::BYTE>(args..., "xor") {}
+   };
+
 }
 
 
